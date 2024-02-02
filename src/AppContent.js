9f2c25b85be5a7e1a46db08f8bf83ebaf38aaf33
katepark1225 <117127078+katepark1225@users.noreply.github.com>
2024-02-02 16:34:07 +0900

    Update AppContent.js

--- src/AppContent.js
+++ src/AppContent.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './index.css'
-import Login from './view/Login'
 import Home from './view/Home'
 
 
@@ -9,7 +8,6 @@ const AppContent = () => {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="login" element={<Login />} />
       <Route path="home" element={<Home />} />
     </Routes>
   )
